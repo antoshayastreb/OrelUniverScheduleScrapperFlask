f63b773023a9e247064fc2ee3a319b819514f732
antoshayastreb <nedanon@outlook.com>
2021-04-09 00:10:25 +0300

    display kurslist onclick

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,15 @@ def get_kourselist():
     return jsonify({'data': render_template('kurslist.html', kurs_list=kurslist_response)})
 
 
+@app.route('/get_grouplist', methods=['POST'])
+def get_grouplist():
+    division_id = request.form['division_id']
+    kurs = request.form['kurs']
+    grouplist_request = base_request + str(division_id) + '/' + str(kurs) + '/' + 'grouplist'
+    grouplist_response = ast.literal_eval(requests.get(grouplist_request).text)
+    return jsonify({'data': render_template('grouplist.html', group_list=grouplist_response)})
+
+
 def get_divisionlist():
     divisions_request = base_request + 'divisionlistforstuds'
 
--- app/static/js/schedule.js
+++ app/static/js/schedule.js
@@ -4,8 +4,29 @@ $(document).ready(function ()
 });
 
 function general() {
-    $('class.tab-content class.accordion-body class.btn').on("click", function(){
+    $('.btn').on('click', function(){
         var el = $(this);
-        el.addClass('active').siblings().removeClass('active');
+        console.log(el.parent().attr('id'));
+        el.siblings().addClass('disabled');
+        var val = el.data('value');
+        switch(el.parent().attr('id')){
+            case 'divisions':
+                $('#kurs .accordion-body').children().remove();
+                kurslist(val);
+                $('#accordionKurs').click()
+
+        }
     })
+}
+
+function kurslist(division_id){
+    $.ajax({
+       url: "/get_kurslist",
+       type: "POST",
+       dataType: "json",
+       data: {'division_id':division_id},
+       success: function(resp){
+         $('#kurs .accordion-body').append(resp.data);
+       }
+      });
 }
