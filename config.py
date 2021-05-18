712e751ca5afa2ac8eb20fd05048277bb2028f1d
antoshayastreb <nedanon@outlook.com>
2021-05-19 00:03:56 +0300

    update config for secret_key

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     # GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
     # GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
     # GOOGLE_DISCOVERY_URL = (
